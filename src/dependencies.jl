77bf10df1c56bd6c83b15dced5bb1a2b0f840a9f
Steven G. Johnson <stevenj@mit.edu>
2015-04-24 22:10:49 -0400

    silence another 0.4 warning

--- src/dependencies.jl
+++ src/dependencies.jl
@@ -825,7 +825,7 @@ macro install (_libmaps...)
                         """
                         # Macro to load a library
                         macro checked_lib(libname, path)
-                            ((VERSION >= v"0.4.0-dev+3844" ? Base.Libdl.dlopen_e : dlopen_e)(path) == C_NULL) && error("Unable to load \\n\\n\$libname (\$path)\\n\\nPlease re-run Pkg.build(package), and restart Julia.")
+                            ((VERSION >= v"0.4.0-dev+3844" ? Base.Libdl.dlopen_e : Base.dlopen_e)(path) == C_NULL) && error("Unable to load \\n\\n\$libname (\$path)\\n\\nPlease re-run Pkg.build(package), and restart Julia.")
                             quote const \$(esc(libname)) = \$path end
                         end
                         """)
