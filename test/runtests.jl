550f39e7ca1c7a6671bf98cec1de8827560f0f18
Iain Dunning <iaindunning@gmail.com>
2015-07-06 11:46:57 -0400

    Test double build

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,5 @@ Pkg.add("Cairo")  # Tests apt-get code paths
 using Cairo
 Pkg.add("HttpParser")  # Tests build-from-source code paths
 using HttpParser
+Pkg.add("ECOS")  # Build-from-source
+Pkg.build("ECOS")  # Double-build it
