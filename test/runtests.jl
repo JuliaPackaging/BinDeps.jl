7f772af902c51f9e03b2c4be98b3f3074568f8c2
Tony Kelman <tony@kelman.net>
2016-07-20 23:10:49 -0400

    Add testing against GSL

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,8 @@ Pkg.build("Cairo")  # Tests apt-get code paths
 using Cairo
 Pkg.build("HttpParser")  # Tests build-from-source code paths
 using HttpParser
+Pkg.build("GSL")  # Tests old-style @load_dependencies, at least on 0.3
+using GSL
 
 # PR 171
 @test BinDeps.lower(nothing, nothing) === nothing
