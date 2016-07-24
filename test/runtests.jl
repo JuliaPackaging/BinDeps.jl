d2777f762912ca94db433d4f2017ada82e50416d
Tony Kelman <tony@kelman.net>
2016-07-23 20:49:58 -0700

    Drop 0.3 support (#238)
    * Drop 0.3 support
    
    latest updates have started triggering mysterious
    'new not defined' errors on PackageEvaluator for a
    few packages, and rather than debug those I think
    we may as well drop 0.3 support and make the latest
    tags 0.4-only.
    
    * fix test/REQUIRE on windows for gsl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,10 +8,8 @@ Pkg.build("Cairo")  # Tests apt-get code paths
 using Cairo
 Pkg.build("HttpParser")  # Tests build-from-source code paths
 using HttpParser
-if is_unix()
-    Pkg.build("GSL")  # Tests old-style @load_dependencies, at least on 0.3
-    using GSL
-end
+Pkg.build("GSL")
+using GSL
 
 # PR 171
 @test BinDeps.lower(nothing, nothing) === nothing
