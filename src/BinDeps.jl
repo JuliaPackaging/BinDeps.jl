e2384488f2373b2385e92f5180032b87e7b0bb21
Jonathan Anderson <JonathanAnderson@users.noreply.github.com>
2017-08-01 10:34:08 -0500

    use ENV["NOSUDO"]=true to prevent `sudo -V` check

--- src/BinDeps.jl
+++ src/BinDeps.jl
@@ -572,7 +572,7 @@ include("show.jl")
 
 const has_sudo = Ref{Bool}(false)
 function __init__()
-    has_sudo[] = try success(`sudo -V`) catch err false end
+    has_sudo[] = get(ENV, "NOSUDO", false) != "true" && try success(`sudo -V`) catch err false end
 end
 
 end
