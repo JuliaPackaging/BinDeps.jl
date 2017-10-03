9ec223a2bd6f0328ceb57599741009ce3d0d5c9a
Alex Arslan <ararslan@comcast.net>
2017-10-03 14:04:35 -0700

    Add glibc_version for checking the glibc version

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,3 +14,18 @@ using GSL
 
 # PR 271
 BinDeps.debug("Cairo")
+
+let gv = glibc_version()
+    if Compat.Sys.islinux()
+        lddv = lowercase(readchomp(`ldd --version`))
+        if contains(lddv, "gnu") || contains(lddv, "glibc")
+            @test isa(gv, VersionNumber)
+            @test gv >= v"1.0.0"
+        else
+            # Assume non-glibc
+            @test gv === nothing
+        end
+    else
+        @test gv === nothing
+    end
+end
