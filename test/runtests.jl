2dc2ced8d0345126b86cb504a27f27e904c60b91
Alex Arslan <ararslan@comcast.net>
2017-10-03 20:29:08 -0700

    Work around Base.Test move to stdlib

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
-using Base.Test
 using Compat
 using BinDeps
+if VERSION < v"0.7.0-DEV.2004"
+    using Base.Test
+else
+    using Test
+end
 
 Pkg.build("Cairo")  # Tests apt-get code paths
 using Cairo
